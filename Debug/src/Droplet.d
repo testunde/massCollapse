a2d3e460fe11f735922bda8ef1647665cb5d79d3
Toby Newman <tobias-riesa@gmx.de>
2018-12-29 00:06:12 +0100

    Update .gitignore for Eclipse IDE files + built binaries

--- Debug/src/Droplet.d
+++
@@ -1,5 +0,0 @@
-src/Droplet.o: ../src/Droplet.cpp ../src/Droplet.h ../src/Global.h
-
-../src/Droplet.h:
-
-../src/Global.h:
--- Debug/src/main.d
+++
@@ -1,5 +0,0 @@
-src/main.o: ../src/main.cpp ../src/Global.h ../src/Droplet.h
-
-../src/Global.h:
-
-../src/Droplet.h:
