caea27750c7ff044ab770ac157c9fbb438ab0ea6
Toby Newman <tobias-riesa@gmx.de>
2019-01-12 16:35:57 +0100

    Add RungeKutta5 method

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -35,11 +35,51 @@ vector<double> Particle::accelByDistance(vector<double> distance, double mass) {
 vector<double> Particle::addVec(vector<double> v1, vector<double> v2) {
     vector<double> result = {v1[0] + v2[0], v1[1] + v2[1]};
     return result;
-    ;
+}
+
+vector<double> Particle::addVec(vector<double> v1, vector<double> v2,
+                                vector<double> v3) {
+    vector<double> result = {v1[0] + v2[0] + v3[0], v1[1] + v2[1] + v3[1]};
+    return result;
 }
 
 vector<double> Particle::multVec(vector<double> v1, double scalar) {
     vector<double> result = {v1[0] * scalar, v1[1] * scalar};
+    return result;
+}
+
+vector<double> Particle::RungeKutta4(vector<double> distance, double mass) {
+    vector<double> F1 = accelByDistance(distance, mass);
+    vector<double> F2 =
+        accelByDistance(addVec(distance, multVec(F1, .5)), mass);
+    vector<double> F3 =
+        accelByDistance(addVec(distance, multVec(F2, .5)), mass);
+    vector<double> F4 = accelByDistance(addVec(distance, F3), mass);
+
+    vector<double> result = {(F1[0] + 2. * (F2[0] + F3[0]) + F4[0]) / 6.,
+                             (F1[1] + 2. * (F2[1] + F3[1]) + F4[1]) / 6.};
+    return result;
+}
+
+vector<double> Particle::RungeKutta5(vector<double> distance, double mass) {
+    vector<double> F1 = accelByDistance(distance, mass);
+    vector<double> F2 =
+        accelByDistance(addVec(distance, multVec(F1, .25)), mass);
+    vector<double> F3 = accelByDistance(
+        addVec(distance, multVec(F1, .125), multVec(F2, .125)), mass);
+    vector<double> F4 =
+        accelByDistance(addVec(distance, multVec(F2, -.5), F3), mass);
+    vector<double> F5 = accelByDistance(
+        addVec(distance, multVec(F1, 3. / 16.), multVec(F4, 9. / 16.)), mass);
+    vector<double> F6 = accelByDistance(
+        addVec(addVec(distance, multVec(F1, -3. / 7.), multVec(F2, 2. / 7.)),
+               addVec(multVec(F3, 12. / 7.), multVec(F4, -12. / 7.),
+                      multVec(F5, 8. / 7.))),
+        mass);
+
+    vector<double> result = {
+        (7. * (F1[0] + F6[0]) + 32. * (F3[0] + F5[0]) + 12. * F4[0]) / 90.,
+        (7. * (F1[1] + F6[1]) + 32. * (F3[1] + F5[1]) + 12. * F4[1]) / 90.};
     return result;
 }
 
@@ -58,15 +98,10 @@ void Particle::updateVelocity() {
 
         printf("%f, %f\n", deltaR[0], deltaR[0]);
 
-        vector<double> F1 = accelByDistance(deltaR, massP);
-        vector<double> F2 =
-            accelByDistance(addVec(deltaR, multVec(F1, .5)), massP);
-        vector<double> F3 =
-            accelByDistance(addVec(deltaR, multVec(F2, .5)), massP);
-        vector<double> F4 = accelByDistance(addVec(deltaR, F3), massP);
+        vector<double> rk = RungeKutta5(deltaR, massP);
 
-        this->velocity[0] += (F1[0] + 2. * (F2[0] + F3[0]) + F4[0]) / 6;
-        this->velocity[1] += (F1[1] + 2. * (F2[1] + F3[1]) + F4[1]) / 6;
+        this->velocity[0] += rk[0];
+        this->velocity[1] += rk[1];
     }
 }
 
