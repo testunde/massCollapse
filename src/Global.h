c44fdda99d065130b162f7441c1604fab87fbeea
Toby Newman <tobias-riesa@gmx.de>
2019-01-22 21:36:19 +0100

    Add initial-velocity-function option

--- src/Global.h
+++ src/Global.h
@@ -15,6 +15,8 @@
 #define ENVIRONMENT_SPAWN_PARTICLE_MASS_STD_2 1.8E1 // [kg] (2*std)
 #define ENVIRONMENT_SPAWN_START_ANGULAR_VELO                                   \
     -2.E-3 // [m/2] (orthogonal towards the center)
+#define ENVIRONMENT_SPAWN_FUNCTIONAL_ANGULAR_VELO_FUNCTION                     \
+    0 // 0: plain angular function (alpha * radius), 1: sqrt(GM*radius)*2
 #define ENVIRONMENT_SPAWN_FORM                                                 \
     2 // 0: square, 1: ellipse, 2: concentrated ellipse, 3: spiral galaxy
 
